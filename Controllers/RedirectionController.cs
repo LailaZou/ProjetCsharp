5d6f6a5cf934fb186faf790480a03be278272d72
LailaZou <laila.zouaq@gmail.com>
2019-05-29 03:43:47 +0000

    mmmm

---
+++ Controllers/RedirectionController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Projet3.Controllers
+{
+    [Authorize]
+    public class RedirectionController : Controller
+    {
+        // GET: Redirection
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Authorize(Roles = "Administrateur")]
+
+        public ActionResult IndexAdmin()
+        {
+            return View();
+        }
+        [Authorize(Roles = "User")]
+
+        public ActionResult IndexUser()
+        {
+            return View();
+        }
+    }
+}
